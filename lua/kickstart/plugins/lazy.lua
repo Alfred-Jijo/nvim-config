6bd4f989ff8f05ebaa3238c1edde5369ff2a35aa
Alfred-Jijo <alfred.jijo28@gmail.com>
2024-04-16 22:41:13 +0100

    Inital Commit

---
+++ lua/kickstart/plugins/lazy.lua
@@ -0,0 +1,117 @@
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+    {
+        'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+    },
+
+    -- Use `opts = {}` to force a plugin to be loaded.
+    --
+    --  This is equivalent to:
+    --    require('Comment').setup({})
+
+    { -- Adds git related signs to the gutter, as well as utilities for managing changes
+        'lewis6991/gitsigns.nvim',
+        opts = {
+            signs = {
+                add = { text = '+' },
+                change = { text = '~' },
+                delete = { text = '_' },
+                topdelete = { text = '‾' },
+                changedelete = { text = '~' },
+            },
+        },
+    },
+
+    -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
+    { -- Useful plugin to show you pending keybinds.
+        'folke/which-key.nvim',
+        event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+        config = function() -- This is the function that runs, AFTER loading
+            require('which-key').setup()
+
+            -- Document existing key chains
+            require('which-key').register {
+                ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+                ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+                ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+                ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+                ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+            }
+        end,
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        init = function()
+            vim.cmd.colorscheme("catppuccin-frappe")
+        end,
+        priority = 1000
+    },
+    -- {
+    --     'folke/tokyonight.nvim',
+    --     init = function()
+    --         vim.cmd.colorscheme("tokyonight-day")
+    --     end,
+    --     priority = 1000, -- Make sure to load this before all the other start plugins.
+    -- },
+    --
+    -- Highlight todo, notes, etc in comments
+    { 'folke/todo-comments.nvim',  event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+
+    { -- Collection of various small independent plugins/modules
+        'echasnovski/mini.nvim',
+        config = function()
+            -- Better Around/Inside textobjects
+            --
+            -- Examples:
+            --  - va)  - [V]isually select [A]round [)]paren
+            --  - yinq - [Y]ank [I]nside [N]ext [']quote
+            --  - ci'  - [C]hange [I]nside [']quote
+            require('mini.ai').setup { n_lines = 500 }
+
+            -- Add/delete/replace surroundings (brackets, quotes, etc.)
+            --
+            -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+            -- - sd'   - [S]urround [D]elete [']quotes
+            -- - sr)'  - [S]urround [R]eplace [)] [']
+            require('mini.surround').setup()
+
+			require("mini.comment").setup()
+
+            -- Simple and easy statusline.
+            --  You could remove this setup call if you don't like it,
+            --  and try some other statusline plugin
+            local statusline = require 'mini.statusline'
+            -- set use_icons to true if you have a Nerd Font
+            statusline.setup { use_icons = vim.g.have_nerd_font }
+
+            -- You can configure sections in the statusline by overriding their
+            -- default behavior. For example, here we set the section for
+            -- cursor location to LINE:COLUMN
+            ---@diagnostic disable-next-line: duplicate-set-field
+            statusline.section_location = function()
+                return '%2l:%-2v'
+            end
+
+            -- ... and there is more!
+            --  Check out: https://github.com/echasnovski/mini.nvim
+        end,
+    },
+
+
+    -- require 'kickstart.plugins.debug',
+    require 'kickstart.plugins.indent_line',
+    require 'kickstart.plugins.treesitter',
+    require 'kickstart.plugins.telescope',
+    require 'kickstart.plugins.lsp',
+    require 'kickstart.plugins.autocompletion',
+    -- require 'kickstart.plugins.lint',
+
+})
